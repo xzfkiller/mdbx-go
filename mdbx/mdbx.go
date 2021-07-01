993e0cf1ea5a6256c09b7ee7287cfc147977c86f
xzfkiller <xzfkiller@gmail.com>
2021-07-01 10:26:42 +0800

    add libmdbx.a, give up on source code, go build is not a system for c build

--- mdbx/mdbx.go
+++ mdbx/mdbx.go
@@ -2,7 +2,6 @@ package mdbx
 
 /*
 #cgo CFLAGS: -pthread -W -Wall -Wno-unused-parameter -Wno-format-extra-args -Wbad-function-cast -Wno-missing-field-initializers -O2 -g
-#cgo linux,pwritev CFLAGS: -DMDB_USE_PWRITEV
 #cgo LDFLAGS: -L../lib -lmdbx -Wl,-rpath=../lib
 
 #include "mdbx.h"
